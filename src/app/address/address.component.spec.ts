07aca1ec12c0e18f40e99a4720a24f1c5fab31f5
Hojjat <h.galeh@gmail.com>
2020-08-12 15:05:12 +0430

    cleared test errors

--- src/app/address/address.component.spec.ts
+++ src/app/address/address.component.spec.ts
@@ -18,8 +18,4 @@ describe('AddressComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'asset-manager'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('asset-manager');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('asset-manager app is running!');
-  });
 });
